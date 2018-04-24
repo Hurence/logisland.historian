45efd4880abb987fbf01476a5ef35bab9a8bfb61
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2018-04-24 09:09:17 +0200

    setup bootstrap and nebular@theme.

--- historian/frontend/src/app/app.module.ts
+++ historian/frontend/src/app/app.module.ts
@@ -1,16 +1,19 @@
+//angular modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+//components
 import { AppComponent } from './app.component';
-
+//nebular modules
+import { NbThemeModule } from '@nebular/theme';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NbThemeModule.forRoot({ name: 'default' }), // this will enable the default theme, you can change this to `cosmic` to enable the dark theme
   ],
   providers: [],
   bootstrap: [AppComponent]
