f76fad0a24fb763c642b5261a48ede8c76b5ec2e
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2018-04-24 16:01:01 +0200

    integrated keycloak to frontend app.

---
+++ historian/frontend/src/app/app-init.ts
@@ -0,0 +1,36 @@
+
+/*
+  Authentificatuion utility
+*/
+
+import { KeycloakService } from 'keycloak-angular';
+ 
+export function initializer(keycloak: KeycloakService): () => Promise<any> {
+  return (): Promise<any> => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        await keycloak.init({
+          config: {
+            "realm": "logisland",
+            "url": "http://keycloak:8080/auth",        
+            "clientId": "logisland-historian",
+            "credentials": {
+              "secret": "4509b60c-4489-4fef-a24e-1ce9377b7d77"
+            }
+          },
+          initOptions: {
+            onLoad: 'login-required',
+            checkLoginIframe: false
+          },
+          bearerExcludedUrls: [
+            '/assets',
+            '/clients/public'
+          ],
+        });
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  };
+}
--- historian/frontend/src/app/app.module.ts
+++ historian/frontend/src/app/app.module.ts
@@ -1,11 +1,14 @@
 // angular modules
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule , APP_INITIALIZER} from '@angular/core';
 import { RouterModule } from '@angular/router'; // we also need angular router for Nebular to function properly
 // components
 import { AppComponent } from './app.component';
 // router
 import { AppRoutingModule } from './app-routing.module';
+// keycloak-angular
+import { KeycloakService, KeycloakAngularModule } from 'keycloak-angular';
+import { initializer } from './app-init';
 // nebular modules
 import { NbThemeModule } from '@nebular/theme';
 import { NbSidebarModule, NbLayoutModule, NbSidebarService } from '@nebular/theme';
@@ -20,8 +23,17 @@ import { NbSidebarModule, NbLayoutModule, NbSidebarService } from '@nebular/them
     AppRoutingModule,
     NbLayoutModule,
     NbSidebarModule,
+    KeycloakAngularModule,
   ],
-  providers: [NbSidebarService],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializer,
+      multi: true,
+      deps: [KeycloakService]
+    },
+    NbSidebarService
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
