ab8637034061fac81d9f1396e48ed9b7d2bf56fc
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2018-04-24 16:51:49 +0200

    Cleanned file to comply with ng lint norms.

--- historian/frontend/src/app/app-init.ts
+++ historian/frontend/src/app/app-init.ts
@@ -4,18 +4,18 @@
 */
 
 import { KeycloakService } from 'keycloak-angular';
- 
+
 export function initializer(keycloak: KeycloakService): () => Promise<any> {
   return (): Promise<any> => {
     return new Promise(async (resolve, reject) => {
       try {
         await keycloak.init({
           config: {
-            "realm": "logisland",
-            "url": "http://keycloak:8080/auth",        
-            "clientId": "logisland-historian",
-            "credentials": {
-              "secret": "4509b60c-4489-4fef-a24e-1ce9377b7d77"
+            realm: 'logisland',
+            url: 'http://keycloak:8080/auth',
+            clientId: 'logisland-historian',
+            credentials: {
+              secret: '4509b60c-4489-4fef-a24e-1ce9377b7d77'
             }
           },
           initOptions: {
