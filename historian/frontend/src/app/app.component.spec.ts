6db31b5511e87f19a6831a5ebff928887c2a2b8f
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2018-04-24 11:26:36 +0200

    Adapted first page angular app starter with ngx-admin framework.

--- historian/frontend/src/app/app.component.spec.ts
+++ historian/frontend/src/app/app.component.spec.ts
@@ -1,11 +1,31 @@
 import { TestBed, async } from '@angular/core/testing';
+
+// angular modules
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'; // we also need angular router for Nebular to function properly
+// components
 import { AppComponent } from './app.component';
+// router
+import { AppRoutingModule } from './app-routing.module';
+// nebular modules
+import { NbThemeModule } from '@nebular/theme';
+import { NbSidebarModule, NbLayoutModule, NbSidebarService } from '@nebular/theme';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        BrowserModule,
+        NbThemeModule.forRoot({ name: 'default' }), // default theme, available[`cosmic`]
+        AppRoutingModule,
+        NbLayoutModule,
+        NbSidebarModule,
+      ],
+      providers: [NbSidebarService]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
--- historian/frontend/src/app/app.module.ts
+++ historian/frontend/src/app/app.module.ts
@@ -1,11 +1,14 @@
-//angular modules
+// angular modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-//components
+import { RouterModule } from '@angular/router'; // we also need angular router for Nebular to function properly
+// components
 import { AppComponent } from './app.component';
-//nebular modules
+// router
+import { AppRoutingModule } from './app-routing.module';
+// nebular modules
 import { NbThemeModule } from '@nebular/theme';
+import { NbSidebarModule, NbLayoutModule, NbSidebarService } from '@nebular/theme';
 
 @NgModule({
   declarations: [
@@ -13,9 +16,12 @@ import { NbThemeModule } from '@nebular/theme';
   ],
   imports: [
     BrowserModule,
-    NbThemeModule.forRoot({ name: 'default' }), // this will enable the default theme, you can change this to `cosmic` to enable the dark theme
+    NbThemeModule.forRoot({ name: 'default' }), // default theme, available[`cosmic`]
+    AppRoutingModule,
+    NbLayoutModule,
+    NbSidebarModule,
   ],
-  providers: [],
+  providers: [NbSidebarService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
