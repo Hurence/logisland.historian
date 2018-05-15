976c0b11ea9720d907de4ed237e69b18f9171f69
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2018-05-15 12:43:44 +0200

    Now search filter wait 400ms before sending filter value.

--- historian/frontend/src/app/datasource/datasource-search/datasource-search.component.ts
+++ historian/frontend/src/app/datasource/datasource-search/datasource-search.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-datasource-search',
@@ -10,15 +12,28 @@ export class DatasourceSearchComponent implements OnInit {
 
   @Output() queryString = new EventEmitter<string>();
   private filterString: string;
+  private searchTerms = new Subject<string>();
 
   constructor() { }
 
   ngOnInit() {
-
+    this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(400),
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+       // switch to new search observable each time the term changes
+       switchMap(
+        (term: string) => {
+          this.queryString.emit(term)
+          return term;
+        }
+      ),
+    ).subscribe(t => console.debug('typed "' + t + '" in search'));
   }
   
-  update(value: string) { 
-    this.queryString.emit(value);
-    this.filterString = value;
+  update(term: string): void {
+    this.searchTerms.next(term);
+    this.filterString = term;
   }
 }
