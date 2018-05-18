1f7c98e5c66424a700a9ec84d2e3ba38167aab8b
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2018-05-18 14:23:30 +0200

    Implemented logout.

--- historian/frontend/src/app/app.component.ts
+++ historian/frontend/src/app/app.component.ts
@@ -11,9 +11,12 @@ export class AppComponent implements OnInit {
 
   private profil: Keycloak.KeycloakProfile;
   private title = 'logisland historian';
+  private logoutUrl: string;
 
   constructor(private keycloakService: KeycloakService,
-              private profilService: ProfilService) {}
+              private profilService: ProfilService) {
+                this.logoutUrl = "http://keycloak:8080/auth/realms/logisland/protocol/openid-connect/logout?redirect_uri=http://localhost:4200/";
+              }
 
   public ngOnInit(): void {
     this.keycloakService.loadUserProfile().then(profil => this.profil = profil);
