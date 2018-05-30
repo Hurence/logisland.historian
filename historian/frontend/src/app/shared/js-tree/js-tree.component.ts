dc87b5404ad205f7a6981313cf1e3543aa60fff0
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2018-05-30 09:40:37 +0200

    Workaround for avoiding property 'jstree' os not known

--- historian/frontend/src/app/shared/js-tree/js-tree.component.ts
+++ historian/frontend/src/app/shared/js-tree/js-tree.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef, Input, SimpleChanges } from '@angular/core';
 // import $ from 'jquery';
-import "jquery"; 
+import "jquery";
 declare const $: JQueryStatic;
 
 @Component({
@@ -15,52 +15,54 @@ export class JsTreeComponent implements OnInit {
 
     constructor() { }
 
-    ngOnInit() {        
+    ngOnInit() {
     }
 
     ngOnChanges(changes: SimpleChanges) {
         console.log('tree changes : ', changes);
-        if (changes.jsonData  && changes.jsonData.previousValue !== changes.jsonData.currentValue) this.createDataTree();
-      }
+        if (changes.jsonData && changes.jsonData.previousValue !== changes.jsonData.currentValue) this.createDataTree();
+    }
 
-    createDataTree() {    
+    createDataTree() {
         if (this.jsonData) {
             console.log('initialization of tree with :', this.jsonData);
-            let tree = $(this.dataTree.nativeElement).jstree({
-                "core": {
-                    "animation": 0,
-                    "check_callback": true,
-                    "themes": {
-                        "stripes": true
+            let tree = ($(this.dataTree.nativeElement) as any)
+            // let tree = $(this.dataTree.nativeElement) TODO find a way to have the method available.
+                .jstree({
+                    "core": {
+                        "animation": 0,
+                        "check_callback": true,
+                        "themes": {
+                            "stripes": true
+                        },
+                        'data': this.jsonData
                     },
-                    'data': this.jsonData
-                },
-                "types": {
-                    "#": {
-                        "max_children": 1,
-                        "max_depth": 4,
-                        "valid_children": ["root"]
+                    "types": {
+                        "#": {
+                            "max_children": 1,
+                            "max_depth": 4,
+                            "valid_children": ["root"]
+                        },
+                        "default": {
+                            "valid_children": ["default", "file"]
+                        },
+                        "file": {
+                            "icon": "fa fa-file",
+                            "valid_children": []
+                        }
                     },
-                    "default": {
-                        "valid_children": ["default", "file"]
+                    "plugins": ["search"],
+                    "search": {
+                        "case_sensitive": false,
+                        "show_only_matches": true,
+                        "show_only_matches_children": false,
+                        "close_opened_onclear": true
                     },
-                    "file": {
-                        "icon": "fa fa-file",
-                        "valid_children": []
-                    }
-                },
-                "plugins": ["search"],
-                "search": {
-                    "case_sensitive": false,
-                    "show_only_matches": true,
-                    "show_only_matches_children":false,
-                    "close_opened_onclear":true
-                },
-                // "plugins": ["checkbox", "contextmenu", "dnd", "search", "sort", "state", "types", "unique", "wholerow"]
-                // "plugins": ["dnd", "search", "sort", "types", "unique", "wholerow", "json_data", "changed"]
-            });
+                    // "plugins": ["checkbox", "contextmenu", "dnd", "search", "sort", "state", "types", "unique", "wholerow"]
+                    // "plugins": ["dnd", "search", "sort", "types", "unique", "wholerow", "json_data", "changed"]
+                })
         }
-        
+
     }
 
 
