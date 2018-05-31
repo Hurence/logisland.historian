21bf145a7430ead265547bc49760bca770e1143e
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2018-05-31 12:14:07 +0200

    Implemented two separate type of tags, HistorianTag and OpcTag, both implementing ITag interface.

---
+++ historian/frontend/src/app/tag/HistorianTag.ts
@@ -0,0 +1,23 @@
+import { Tag, ITag } from "./tag";
+
+
+export interface IHistorianTag extends ITag {
+    labels: string[];
+    description: string;
+    update_rate: number;
+}
+
+export class HistorianTag extends Tag implements IHistorianTag  {
+
+    labels: string[];
+    description: string;
+    update_rate: number;
+
+    constructor(options: IHistorianTag = {
+        description: '',
+        labels: [],
+        update_rate: 0,
+      }) {
+          super(options);
+    }
+}
