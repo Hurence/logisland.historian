01b9fab996324e3160d1800e83c2ce5eb7e1abd5
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2018-06-13 18:06:38 +0200

    Automatically refresh stats from tags in tabular screen.

--- historian/frontend/src/app/tag/tag-dataview/tag-cell-component/tag-cell-component.component.ts
+++ historian/frontend/src/app/tag/tag-dataview/tag-cell-component/tag-cell-component.component.ts
@@ -1,24 +1,53 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 
 import { Measures } from '../../../measure/Measures';
 import { MeasuresService } from '../../../measure/measures.service';
 import { IHistorianTag } from '../../modele/HistorianTag';
+import { Subscription, Observable, Subject } from 'rxjs';
+import { interval } from 'rxjs/observable/interval';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-tag-cell-component',
   templateUrl: './tag-cell-component.component.html',
   styleUrls: ['./tag-cell-component.component.css']
 })
-export class TagCellComponentComponent implements OnInit {
+export class TagCellComponentComponent implements OnInit, OnDestroy {
 
   @Input() tag: IHistorianTag;
+  refreshRate = 3000; // milliseconds
   stats: Measures;
+  statsUpdater: Subscription;
+  lastRefreshed: Date;
+  private resfreshStats = new Subject<Measures>();
 
   constructor(private measuresService: MeasuresService) { }
 
   ngOnInit() {
-    this.measuresService.getStat('series-A').subscribe(stats => {
-      this.stats = stats;
+    this.resfreshStats.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(400),
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+       // switch to new search observable each time the term changes
+      switchMap(
+        (stats: Measures) => {
+          this.stats = stats;
+          return 'stats';
+        }
+      ),
+    ).subscribe();
+
+    this.statsUpdater = interval(this.refreshRate).subscribe(t => {
+      this.lastRefreshed = new Date();
+      this.measuresService.getStat('series-A').subscribe(stats => {
+        this.resfreshStats.next(stats);
+      });
     });
+  }
+
+  ngOnDestroy(): void {
+    if (this.statsUpdater) this.statsUpdater.unsubscribe();
+    if (this.resfreshStats) this.resfreshStats.unsubscribe();
   }
 }
