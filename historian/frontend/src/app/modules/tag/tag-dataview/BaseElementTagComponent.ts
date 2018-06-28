29d54fd58b163ab2ccada65820c149b39920379f
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2018-06-28 17:30:20 +0200

    Search stat of current tag in tag-dataview instead of hardcoded value. Print "no stat found" when server returning error 404.

--- historian/frontend/src/app/modules/tag/tag-dataview/BaseElementTagComponent.ts
+++ historian/frontend/src/app/modules/tag/tag-dataview/BaseElementTagComponent.ts
@@ -4,6 +4,7 @@ import { Measures } from '../../../measure/Measures';
 import { Subscription, Subject, interval } from 'rxjs';
 import { MeasuresService } from '../../../measure/measures.service';
 import { debounceTime, switchMap, distinctUntilChanged } from 'rxjs/operators';
+import { Statement } from '@angular/compiler';
 
 export abstract class BaseElementTagComponent implements OnInit, OnDestroy, OnChanges {
 
@@ -11,17 +12,27 @@ export abstract class BaseElementTagComponent implements OnInit, OnDestroy, OnCh
     @Input() refreshRate: number; // milliseconds
     stats: Measures;
     lastRefreshed: Date;
+    notFound: boolean;
     private resfreshStats = new Subject<number>();
     private initSubscription: Subscription;
     private refreshSubscription: Subscription;
     protected measuresService: MeasuresService;
 
     ngOnInit() {
+        this.notFound = false;
         // initialize stat
-        this.initSubscription = this.measuresService.getStat('series-A').subscribe(stats => {
-            this.lastRefreshed = new Date();
-            this.stats = stats;
-        });
+        this.initSubscription = this.measuresService.getStat(this.tag.id).subscribe(
+            stats => {
+                this.lastRefreshed = new Date();
+                this.notFound = false;
+                this.stats = stats;
+            },
+            error => {
+                if (error.status === 404) {
+                    this.notFound = true;
+                }
+            }
+        );
         // initialize watch of stats with initial refreshrate
         this.refreshSubscription = this.resfreshStats.pipe(
             debounceTime(400),
@@ -31,9 +42,17 @@ export abstract class BaseElementTagComponent implements OnInit, OnDestroy, OnCh
         ).subscribe(
             (t) => {
                 this.lastRefreshed = new Date(); // TODO add a marker for loading
-                this.measuresService.getStat('series-A').subscribe(stats => {
-                    this.stats = stats;
-                });
+                this.notFound = false;
+                this.measuresService.getStat(this.tag.id).subscribe(
+                    stats => {
+                        this.stats = stats;
+                    },
+                    error => {
+                        if (error.status === 404) {
+                            this.notFound = true;
+                        }
+                    }
+                );
             }
         );
         this.resfreshStats.next(this.refreshRate);
