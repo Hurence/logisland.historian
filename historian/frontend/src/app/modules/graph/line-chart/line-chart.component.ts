6b34025f28eed8a8f9456987b20e055767042b4b
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2018-07-02 16:23:01 +0200

    Added button to filter on different period of time in Line Chart component.

--- historian/frontend/src/app/modules/graph/line-chart/line-chart.component.ts
+++ historian/frontend/src/app/modules/graph/line-chart/line-chart.component.ts
@@ -8,6 +8,12 @@ import { ProfilService } from '../../../profil/profil.service';
 import { IHistorianTag } from '../../tag/modele/HistorianTag';
 import { ArrayUtil } from '../../../shared/array-util';
 
+export interface TimeRangeFilter {
+  label: string,
+  start: string,
+  end: string,
+}
+
 @Component({
   selector: 'app-line-chart',
   templateUrl: './line-chart.component.html',
@@ -18,6 +24,38 @@ export class LineChartComponent extends AbsSubscriberToSelectionOfTag implements
   data: ILineChartData;
   options: ILineChartOption;
   tags: IHistorianTag[];
+  timeRangeFilter: TimeRangeFilter =  {label: 'Defaut', start: '1474399200000', end: '1474499500000'};
+
+  starts: TimeRangeFilter[] = [
+    {label: 'Today', start: 'NOW/DAY', end: 'NOW+1DAY/DAY'},
+    {label: 'This week', start: 'NOW/WEEK', end: 'NOW+1WEEK/WEEK'},
+    {label: 'This month', start: 'NOW/MONTH', end: 'NOW+1MONTH/MONTH'},
+    {label: 'This year', start: 'NOW/YEAR', end: 'NOW+1YEAR/YEAR'},
+    {label: 'The day so far', start: 'NOW/DAY', end: 'NOW'},
+    {label: 'Week to date', start: 'NOW/WEEK', end: 'NOW'},
+    {label: 'Month to date', start: 'NOW/MONTH', end: 'NOW'},
+    {label: 'Year to date', start: 'NOW/YEAR', end: 'NOW'}, // yyyy-mm-ddThh:mm:ss.mmmZ
+    {label: 'Yesterday', start: 'NOW-1DAY/DAY', end: 'NOW/DAY'},
+    {label: 'Day before yesterday', start: 'NOW-2DAYS/DAY', end: 'NOW-1DAY/DAY'},
+    {label: 'This day last week', start: 'NOW-7DAYS/DAY', end: 'NOW-6DAYS/DAY'},
+    {label: 'Previous week', start: 'NOW-1WEEK/WEEK', end: 'NOW/WEEK'},
+    {label: 'Previous month', start: 'NOW-1MONTH/MONTH', end: 'NOW/MONTH'},
+    {label: 'Previous year', start: 'NOW-1YEAR/YEAR', end: 'NOW/YEAR'},
+    {label: 'Last 15 minutes', start: 'NOW-15MINUTES', end: 'NOW'},
+    {label: 'Last 30 minutes', start: 'NOW-30MINUTES', end: 'NOW'},
+    {label: 'Last 1 hour', start: 'NOW-1HOUR', end: 'NOW'},
+    {label: 'Last 4 hours', start: 'NOW-4HOURS', end: 'NOW'},
+    {label: 'Last 12 hours', start: 'NOW-12HOURS', end: 'NOW'},
+    {label: 'Last 24 hours', start: 'NOW-24HOURS', end: 'NOW'},
+    {label: 'Last 7 days', start: 'NOW-7DAYS', end: 'NOW'},
+    {label: 'Last 30 days', start: 'NOW-30DAYS', end: 'NOW'},
+    {label: 'Last 60 days', start: 'NOW-60DAYS', end: 'NOW'},
+    {label: 'Last 90 days', start: 'NOW-90DAYS', end: 'NOW'},
+    {label: 'Last 6 months', start: 'NOW-6MONTHS', end: 'NOW'},
+    {label: 'Last 1 year', start: 'NOW-1YEAR', end: 'NOW'},
+    {label: 'Last 2 years', start: 'NOW-2YEARS', end: 'NOW'},
+    {label: 'Last 5 year', start: 'NOW-5YEARS', end: 'NOW'}
+  ]
 
   constructor(private measuresService: MeasuresService,
               private arrayUtil: ArrayUtil,
@@ -67,7 +105,7 @@ export class LineChartComponent extends AbsSubscriberToSelectionOfTag implements
         const request = this.buildTagMeasureRequest(tag);
         this.measuresService.get(request).subscribe(m => {
           this.data.datasets.push(this.convertMeasureToDataset(m));
-          this.update();
+          this.redrawGraph();
         });
       });
     });
@@ -76,17 +114,33 @@ export class LineChartComponent extends AbsSubscriberToSelectionOfTag implements
       const request = this.buildTagMeasureRequest(tag);
       this.measuresService.get(request).subscribe(m => {
         this.data.datasets.push(this.convertMeasureToDataset(m));
-        this.update();
+        this.redrawGraph();
       });
     });
     this.removeTagSubscription = this.profilService.getRemoveTagPublisher().subscribe(tag => {
       this.arrayUtil.remove(this.tags, elem => tag.id === elem.id);
       this.arrayUtil.remove(this.data.datasets, dataset => tag.id === dataset.label);
-      this.update();
+      this.redrawGraph();
     });
   }
 
-  update() {
+  changeTimeRange(timeRange: TimeRangeFilter) {
+    this.timeRangeFilter = timeRange;
+    this.updateGraphData();
+  }
+
+  updateGraphData() {
+    this.data.datasets = [];
+    this.tags.forEach(tag => {
+      const request = this.buildTagMeasureRequest(tag);
+      this.measuresService.get(request).subscribe(m => {
+        this.data.datasets.push(this.convertMeasureToDataset(m));
+        this.redrawGraph();
+      });
+    })
+  }
+
+  redrawGraph() {
     this.data = Object.assign({}, this.data);
   }
 
@@ -113,8 +167,8 @@ export class LineChartComponent extends AbsSubscriberToSelectionOfTag implements
   private buildTagMeasureRequest(tag: IHistorianTag): MeasuresRequest {
     return new MeasuresRequest({
       itemId: tag.id,
-      start: '1474399200000',
-      end: '1474499500000'
+      start: this.timeRangeFilter.start,
+      end: this.timeRangeFilter.end
     });
   }
 }
