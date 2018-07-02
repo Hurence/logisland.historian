619aca41b30cd6800fd7a588bcafb2fa9750c039
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2018-07-02 16:24:31 +0200

    Fixed a bug in Historian tag treenodes. Parent was not assigned so query builded was wrong.

--- historian/frontend/src/app/modules/tag/historian-tag-tree/ng-treenode.service.ts
+++ historian/frontend/src/app/modules/tag/historian-tag-tree/ng-treenode.service.ts
@@ -16,20 +16,22 @@ export class NgTreenodeService {
         );
     }
     // TODO implement generic version of this
-    private buildTagTreeNodes(nodes: RestTreeNode[], fields: string[], index: number): TreeNode[] {
+    private buildTagTreeNodes(nodes: RestTreeNode[], fields: string[], index: number, parent?: TreeNode): TreeNode[] {
         const currentType: string = fields[index];
         const newIndex = index + 1;
         return nodes.map(node => {
-            const children: TreeNode[] = this.buildTagTreeNodes(node.children, fields, newIndex);
-            return {
+            const currentNode: TreeNode = {
                 label: node.value,
                 data: currentType,
                 expandedIcon: 'fa fa-folder-open',
                 collapsedIcon: 'fa fa-folder',
-                children: children,
                 leaf: false,
                 type: currentType,
             };
+            const children: TreeNode[] = this.buildTagTreeNodes(node.children, fields, newIndex, currentNode);
+            currentNode.children = children;
+            if (parent) currentNode.parent = parent;
+            return currentNode;
         });
     }
 }
