90a589b0aa121f66cba5635da5efb275d9a8eb62
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2018-08-07 09:34:22 +0200

    Moved tag form component into multiple-tag-form directory.

--- historian/frontend/src/app/modules/tag/tag.module.ts
+++ historian/frontend/src/app/modules/tag/tag.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../../shared/shared.module';
-import { TagFormComponent } from './tag-form/tag-form.component';
+import { TagFormComponent } from './tag-form/multiple-tag-form/tag-form.component';
 import { TagHelpComponent } from './tag-help/tag-help.component';
 import { TagDataviewComponent } from './tag-dataview/tag-dataview.component';
 import { TagCellComponent } from './tag-dataview/tag-cell/tag-cell.component';
