d50f20a81455cfa420b1b0c9420029bb2ecbb0ad
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2018-08-10 10:08:53 +0200

    Factorized add-tag-form and edt-tag-form that are basically the sames at the moment.

--- historian/frontend/src/app/modules/tag/tag-form/add-tag-form/add-tag-form.component.ts
+++ historian/frontend/src/app/modules/tag/tag-form/add-tag-form/add-tag-form.component.ts
@@ -7,88 +7,23 @@ import { HistorianTag } from '../../modele/HistorianTag';
 import { TagHistorianService } from '../../service/tag-historian.service';
 import { TagOpcService } from '../../service/tag-opc.service';
 import { ConfirmationService } from 'primeng/api';
+import { BaseSingleTagForm } from '../BaseSingleTagForm';
 
 @Component({
   selector: 'app-add-tag-form',
   templateUrl: './add-tag-form.component.html',
   styleUrls: ['./add-tag-form.component.css']
 })
-export class AddTagFormComponent extends BaseDynamicFormComponent<HistorianTag, HistorianTag> implements OnInit {
-
-  displayFoundMsg = false;
-  displayNotFoundMsg = false;
-  nodeFoundMsd = 'RAS';
-  loading = false;
+export class AddTagFormComponent extends BaseSingleTagForm implements OnInit {
 
   constructor(protected qcs: QuestionControlService,
               protected service: TagHistorianService,
-              protected tagOpcService: TagOpcService,
-              protected confirmationService: ConfirmationService) {
-    super(qcs, service, confirmationService);
+              protected confirmationService: ConfirmationService,
+              protected tagOpcService: TagOpcService) {
+    super(qcs, service, confirmationService, tagOpcService);
   }
 
   ngOnInit() {
     super.ngOnInit();
-    this.copyNodeIdToTagName();
-  }
-
-  onFetchMetaData() {
-    const tagWithNodeId = this.prepareSaveItem();
-    this.displayLoading();
-    this.tagOpcService.searchForTag(tagWithNodeId.datasource_id, tagWithNodeId.node_id).pipe(
-      map(opcTag => new HistorianTag(opcTag))
-    ).subscribe(
-      historianTag => {
-        this.form.reset(historianTag);
-        this.displaySucess();
-        // Questions.modifyQuestions(this.questions, AddTagFormComponent.QUESTION_AFTER_FETCHING_DATA);
-      },
-      error => {
-        // const httpError = (<HttpErrorResponse> error)
-        this.displayError(`No node found with id '${this.form.value.node_id}'`);
-        console.error(JSON.stringify(error));
-      }
-    );
-  }
-
-  resetDisplay(): void {
-    this.displayFoundMsg = false;
-    this.loading = false;
-    this.displayNotFoundMsg = false;
-    // Questions.modifyQuestions(this.questions, AddTagFormComponent.QUESTION_BEFORE_FETCHING_DATA);
-  }
-
-  private copyNodeIdToTagName(): void {
-    this.form.get('node_id').valueChanges.forEach(
-      (nodeId: string) => {
-        this.form.get('tag_name').patchValue(nodeId);
-    });
-  }
-
-  private displayLoading(): void {
-    this.displayNotFoundMsg = false;
-    this.displayFoundMsg = false;
-    this.loading = true;
-  }
-
-  private displaySucess(): void {
-    this.displayNotFoundMsg = false;
-    this.loading = false;
-    this.displayFoundMsg = true;
-  }
-
-  private displayError(error: string): void {
-    this.nodeFoundMsd = error;
-    this.displayFoundMsg = false;
-    this.loading = false;
-    this.displayNotFoundMsg = true;
-  }
-
-  protected create(item: HistorianTag): HistorianTag {
-    return new HistorianTag(item);
-  }
-
-  protected convert(backObj: HistorianTag): HistorianTag {
-    return new HistorianTag(backObj);
   }
 }
