7d584022785750b2c58f21bb435aa72fd00a4f48
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2018-08-11 08:46:31 +0200

    Added an apply button in datasource dashboard to update logisland configuration.

---
+++ historian/frontend/src/app/dataflow.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { environment } from '../environments/environment';
+import { Utilities } from './shared/utilities.service';
+import { MessageService } from 'primeng/components/common/messageservice';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class DataFlowService {
+
+    private baseUrl = `${environment.HISTORIAN_API_URL}dataflows`;
+
+    constructor(protected http: HttpClient, protected help: Utilities,
+      protected messageService: MessageService) {}
+    
+    autoUpdateConfiguration(dataFlowId: string): Observable<any> {
+      return this.http.post(`${this.baseUrl}/${dataFlowId}/auto-update`, {});
+    }
+}
