2467ecaa91e82f8b0dfb65fb0f09a93e1092d7d6
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2018-09-04 15:55:30 +0200

    Improved timeRange selection component. Principally esthetic changes.

--- historian/frontend/src/app/shared/time-range-selection/time-range-selection.component.ts
+++ historian/frontend/src/app/shared/time-range-selection/time-range-selection.component.ts
@@ -15,6 +15,7 @@ export class TimeRangeSelectionComponent implements OnInit {
   timeRangeDisplay: string;
   @Output() timeRangeChange = new EventEmitter<TimeRangeFilter>();
   view: string = 'Quick';
+  showTimeRangeMenu: boolean = false;
   private _from: Date = new Date(0);
   private _to: Date  = new Date();
 
@@ -54,36 +55,12 @@ export class TimeRangeSelectionComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.timeRangeOptions = [
-      {label: 'Today', value: timeRangeBuiltIn.TODAY},
-      // {label: 'This week', start: 'NOW/WEEK', end: 'NOW+1WEEK/WEEK'},
-      {label: 'This month', value: {label: 'This month', start: 'NOW/MONTH', end: 'NOW+1MONTH/MONTH'}},
-      {label: 'This year', value: {label: 'This year', start: 'NOW/YEAR', end: 'NOW+1YEAR/YEAR'}},
-      {label: 'The day so far', value: {label: 'The day so far', start: 'NOW/DAY', end: 'NOW'}},
-      // {label: 'Week to date', start: 'NOW/WEEK', end: 'NOW'},
-      {label: 'Month to date', value: {label: 'Month to date', start: 'NOW/MONTH', end: 'NOW'}},
-      {label: 'Year to date', value: {label: 'Year to date', start: 'NOW/YEAR', end: 'NOW'}}, // yyyy-mm-ddThh:mm:ss.mmmZ
-      {label: 'Yesterday', value: {label: 'Yesterday', start: 'NOW-1DAY/DAY', end: 'NOW/DAY'}},
-      {label: 'Day before yesterday', value: {label: 'Day before yesterday', start: 'NOW-2DAYS/DAY', end: 'NOW-1DAY/DAY'}},
-      {label: 'This day last week', value: {label: 'This day last week', start: 'NOW-7DAYS/DAY', end: 'NOW-6DAYS/DAY'}},
-      // {label: 'Previous week', start: 'NOW-1WEEK/WEEK', end: 'NOW/WEEK'},
-      {label: 'Previous month', value: {label: 'Previous month', start: 'NOW-1MONTH/MONTH', end: 'NOW/MONTH'}},
-      {label: 'Previous year', value: {label: 'Previous year', start: 'NOW-1YEAR/YEAR', end: 'NOW/YEAR'}},
-      {label: 'Last 15 minutes', value: {label: 'Last 15 minutes', start: 'NOW-15MINUTES', end: 'NOW'}},
-      {label: 'Last 30 minutes', value: {label: 'Last 30 minutes', start: 'NOW-30MINUTES', end: 'NOW'}},
-      {label: 'Last 1 hour', value: {label: 'Last 1 hour', start: 'NOW-1HOUR', end: 'NOW'}},
-      {label: 'Last 4 hours', value: {label: 'Last 4 hours', start: 'NOW-4HOURS', end: 'NOW'}},
-      {label: 'Last 12 hours', value: {label: 'Last 12 hours', start: 'NOW-12HOURS', end: 'NOW'}},
-      {label: 'Last 24 hours', value: {start: 'NOW-24HOURS', end: 'NOW'}},
-      {label: 'Last 7 days', value: {start: 'NOW-7DAYS', end: 'NOW'}},
-      {label: 'Last 30 days', value: {start: 'NOW-30DAYS', end: 'NOW'}},
-      {label: 'Last 60 days', value: {start: 'NOW-60DAYS', end: 'NOW'}},
-      {label: 'Last 90 days', value: {start: 'NOW-90DAYS', end: 'NOW'}},
-      {label: 'Last 6 months', value: {start: 'NOW-6MONTHS', end: 'NOW'}},
-      {label: 'Last 1 year', value: {start: 'NOW-1YEAR', end: 'NOW'}},
-      {label: 'Last 2 years', value: {start: 'NOW-2YEARS', end: 'NOW'}},
-      {label: 'Last 5 year', value: {start: 'NOW-5YEARS', end: 'NOW'}}
-    ];
+    this.timeRangeOptions = Object.keys(timeRangeBuiltIn).map(key => {
+      return {
+        label: timeRangeBuiltIn[key].label,
+        value: timeRangeBuiltIn[key],
+      };
+    });
   }
 
   setCustomTimeRange(): void {
@@ -103,5 +80,8 @@ export class TimeRangeSelectionComponent implements OnInit {
       start: startMoment.valueOf().toString(),
       end: endMoment.valueOf().toString(),
     };
+  }
+  toggleShowTimeRangeMenu() {
+    this.showTimeRangeMenu = !this.showTimeRangeMenu;
   }
 }
