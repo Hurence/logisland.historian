c00d47e4f9c4c72178c34f7eef797d6221a9d253
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2018-09-14 07:23:27 +0200

    Use variable in changes instead of class property.

--- historian/frontend/src/app/modules/tag/tag-tree/historian-tag-tree/historian-tag-tree.component.ts
+++ historian/frontend/src/app/modules/tag/tag-tree/historian-tag-tree/historian-tag-tree.component.ts
@@ -41,7 +41,7 @@ export class HistorianTagTreeComponent extends BaseTagTreeComponent implements O
       this.treeNodes = this.treeNodes;
       this.expandAll(false);
     }
-    if (changes.selectedTags && this.selectedTags && this.treeNodes.length !== 0) {
+    if (changes.selectedTags && changes.selectedTags.currentValue && this.treeNodes.length !== 0) {
       this.loadNodeOfTags(this.selectedTags);
       this.selectedNodes.forEach(node => {
         this.restoreNodeTree(node);
