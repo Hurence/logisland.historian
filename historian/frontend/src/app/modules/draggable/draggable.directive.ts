9a870d098ae891c2c5dee4bb90295d584820fee9
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2018-10-31 15:08:55 +0100

    Changed implementation of DraggableDirective.

--- historian/frontend/src/app/modules/draggable/draggable.directive.ts
+++ historian/frontend/src/app/modules/draggable/draggable.directive.ts
@@ -1,89 +1,57 @@
-import { Directive, Output, EventEmitter, HostListener, HostBinding, OnInit, OnDestroy, ElementRef } from "@angular/core";
-import { Subject } from "rxjs";
-import { switchMap, takeUntil, repeat, take } from "rxjs/operators";
+import { Directive, Output, EventEmitter, HostListener, HostBinding, ElementRef } from "@angular/core";
 
 @Directive({
   selector: '[appDraggable],[appDroppable]'
 })
-export class DraggableDirective implements OnInit, OnDestroy {
+export class DraggableDirective {
 
   @HostBinding('class.draggable') draggable = true;
-  @HostBinding('class.dragging') dragging = false;
+
+  pointerId?: number;
+
+  // to trigger pointer-events polyfill
+  @HostBinding('attr.touch-action') touchAction = 'none';
 
   @Output() dragStart = new EventEmitter<PointerEvent>();
   @Output() dragMove = new EventEmitter<PointerEvent>();
   @Output() dragEnd = new EventEmitter<PointerEvent>();
 
-  pointerId?: number;
-  // to trigger pointer-events polyfill
-  @HostBinding('attr.touch-action') touchAction = 'none';
-
-  private pointerDown = new Subject<PointerEvent>();
-  private pointerMove = new Subject<PointerEvent>();
-  private pointerUp = new Subject<PointerEvent>();  
+  @HostBinding('class.dragging') dragging = false;
 
   constructor(protected element: ElementRef) {}
 
   @HostListener('pointerdown', ['$event'])
   onPointerDown(event: PointerEvent): void {
-    event.stopPropagation();
-    this.pointerDown.next(event);    
+    // added after YouTube video: ignore right-click
+    if (event.button !== 0) {
+      return;
+    }
+
+    this.pointerId = event.pointerId;
+
+    this.dragging = true;
+    this.dragStart.emit(event);
   }
 
   @HostListener('document:pointermove', ['$event'])
   onPointerMove(event: PointerEvent): void {
-    this.pointerMove.next(event);
+    if (!this.dragging || event.pointerId !== this.pointerId) {
+      return;
+    }
+
+    this.dragMove.emit(event);
   }
 
+  // added after YouTube video: pointercancel
+  @HostListener('document:pointercancel', ['$event'])
   @HostListener('document:pointerup', ['$event'])
   onPointerUp(event: PointerEvent): void {
-    this.pointerUp.next(event);
-  }
-
-  ngOnInit(): void {
-    this.pointerDown.asObservable()
-      .subscribe(event => {
-        // ignore right-click
-        if (event.button !== 0) {
-          return;
-        }
-        this.pointerId = event.pointerId;        
-        this.dragStart.emit(event);
-      });
-
-    this.pointerDown.pipe(
-      switchMap(() => this.pointerMove),
-      takeUntil(this.pointerUp),
-      repeat()
-    ).subscribe(event => { 
-      if (event.pointerId !== this.pointerId) {
-        return;
-      }
-      this.dragMove.emit(event)
-    });
-
-    this.pointerDown.pipe(
-      switchMap(() => this.pointerUp),
-      take(1),
-      repeat()
-    ).subscribe(event => { 
-      if (event.pointerId !== this.pointerId) {
-        return;
-      }   
-      this.dragEnd.emit(event)
-    });
-  }
-
-  ngOnDestroy(): void {
-    if (this.pointerDown && !this.pointerDown.closed) {
-      this.pointerDown.unsubscribe();
+    if (!this.dragging || event.pointerId !== this.pointerId) {
+      return;
     }
-    if (this.pointerMove && !this.pointerMove.closed) {
-      this.pointerMove.unsubscribe();
-    }
-    if (this.pointerUp && !this.pointerUp.closed) {
-      this.pointerUp.unsubscribe();
-    }
+
+    this.dragging = false;
+    this.dragEnd.emit(event);
   }
 
   getBoundingClientRect(): ClientRect {
